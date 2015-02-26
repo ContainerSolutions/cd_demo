a565db501264e029f7c1c4589a0c89c3e34dc9c6
Pini Reznik <pini42@gmail.com>
2015-02-26 23:18:55 +0100

    link jenkins to marathon

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,18 @@
 
 if [ -z "${1}" ]; then
    version="latest"
+   marathon="localhost"
 else
    version="${1}"
+   marathon=${MARATHON_PORT_8080_TCP_ADDR}
 fi
 
-curl -X DELETE -H "Content-Type: application/json" http://localhost:8080/v2/apps/app 
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/app 
 sleep 1
 #curl -X POST -H "Content-Type: application/json" http://localhost:8080/v2/apps -d@app_marathon.json
 
 cp -f app_marathon.json app_marathon.json.tmp
 sed -i "s/latest/${version}/g" app_marathon.json.tmp
 
-curl -X POST -H "Content-Type: application/json" http://localhost:8080/v2/apps -d@app_marathon.json.tmp
+
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app_marathon.json.tmp
--- nodejs_app/app.js
+++ nodejs_app/app.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World sss\n");
+  response.end("Hello World\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
